a4616c269eced6c5cafb47b940fa04b661d1d925
Javier Pestana <javier.pestana85@gmail.com>
2024-09-29 21:56:19 +0100

    isCustomer computed function and v-if to iterate customers

--- src/views/CustomersView.vue
+++ src/views/CustomersView.vue
@@ -1,14 +1,16 @@
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, ref, computed } from "vue";
 import axios from "axios";
 import RouterLink from "../components/UI/RouterLink.vue";
 import Heading from "@/components/UI/Heading.vue";
+
+const customers = ref([]);
 
 onMounted(() => {
   axios
     .get("http://localhost:4000/customers")
     .then(({ data }) => {
-      console.log(data);
+      customers.value = data;
     })
     .catch((error) => console.log(error));
 });
@@ -19,6 +21,10 @@ defineProps({
     required: true,
   },
 });
+
+const ifCustomers = computed(() => {
+  return customers.value.length > 0;
+});
 </script>
 
 <template>
@@ -26,4 +32,42 @@ defineProps({
     <RouterLink to="add-customer"> Add Customer </RouterLink>
   </div>
   <Heading>{{ title }}</Heading>
+  <div v-if="ifCustomers" class="flow-root mx-auto mt-10 p-5 bg-white shadow">
+    <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="min-w-full py-2 align-middle sm:px-6 lg:px-8">
+        <table class="min-w-full divide-y divide-gray-300">
+          <thead>
+            <tr>
+              <th
+                scope="col"
+                class="p-2 text-left text-sm font-extrabold text-gray-600"
+              >
+                Name
+              </th>
+              <th
+                scope="col"
+                class="p-2 text-left text-sm font-extrabold text-gray-600"
+              >
+                Company
+              </th>
+              <th
+                scope="col"
+                class="p-2 text-left text-sm font-extrabold text-gray-600"
+              >
+                State
+              </th>
+              <th
+                scope="col"
+                class="p-2 text-left text-sm font-extrabold text-gray-600"
+              >
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody class="divide-y divide-gray-200 bg-white"></tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+  <p v-else class="text-center mt-10">There are no customers</p>
 </template>
